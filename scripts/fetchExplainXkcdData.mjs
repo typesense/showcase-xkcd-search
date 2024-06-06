ded5f03acbacb3dd1633decfd51de4204d312dde
Hayden <hayden.hhoang@gmail.com>
2024-06-06 15:54:50 +0700

    feat: implement exponential backoff retries, request batching

--- scripts/fetchExplainXkcdData.mjs
+++ scripts/fetchExplainXkcdData.mjs
@@ -1,34 +1,54 @@
 import 'dotenv/config';
 import fetch from 'node-fetch';
 import fs from 'fs';
-import { fileURLToPath } from 'url';
-import path from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const DATA_DIR = path.resolve(__dirname, '../data/raw');
+import { DATA_DIR } from './utils/path.mjs';
+import { exponentialBackoffRetry, BatchAPICall } from './utils/network.mjs';
 
 let response;
 
 response = await fetch('https://xkcd.com/info.0.json');
 const latestComicId = (await response.json())['num'];
 const comicIds = [...Array(latestComicId + 1).keys()].slice(1);
+const batchAPICall = new BatchAPICall();
 
 for await (const comicId of comicIds) {
   const filePath = `${DATA_DIR}/${comicId}.html`;
   if (fs.existsSync(filePath)) {
     console.log(`Explanation for comic ${comicId} already exists. Skipping.`);
   } else {
-    console.log(`Fetching explanation for comic ${comicId}.`);
-    response = await fetch(
-      `https://www.explainxkcd.com/wiki/index.php/${comicId}`
-    );
-    // 🙏 https://stackoverflow.com/a/51302466/123545
-    const fileStream = fs.createWriteStream(filePath);
-    await new Promise((resolve, reject) => {
-      response.body.pipe(fileStream);
-      response.body.on('error', reject);
-      fileStream.on('finish', resolve);
-    });
+    const request = async () => {
+      console.log(`Fetching explanation for comic ${comicId}.`);
+
+      const fetchExplanation = async () => {
+        const res = await fetch(
+          `https://www.explainxkcd.com/wiki/index.php/${comicId}`
+        );
+        if (!res.ok) throw new Error('Request failed!');
+        return res;
+      };
+
+      try {
+        const response = await exponentialBackoffRetry(fetchExplanation, {
+          callback: ({ attempt, delayMs }) =>
+            console.log(
+              `Retry fetching explanation for comic ${comicId}: attempt ${attempt} after ${delayMs}ms`
+            ),
+        });
+        // 🙏 https://stackoverflow.com/a/51302466/123545
+        const fileStream = fs.createWriteStream(filePath);
+        await new Promise((resolve, reject) => {
+          response.body.pipe(fileStream);
+          response.body.on('error', reject);
+          fileStream.on('finish', resolve);
+        });
+        return `Explanation ${comicId} success`;
+      } catch (error) {
+        console.warn(`Error fetching explanation for comic ${comicId}`);
+        return `Explanation ${comicId} failed`;
+      }
+    };
+    batchAPICall.requestList.push(request);
   }
 }
+
+batchAPICall.makeRequests();
--- scripts/fetchXkcdData.mjs
+++ scripts/fetchXkcdData.mjs
@@ -1,32 +1,60 @@
-import 'dotenv/config'
+import 'dotenv/config';
 import fetch from 'node-fetch';
 import fs from 'fs';
-import { fileURLToPath } from 'url';
-import path from 'path';
+import { DATA_DIR } from './utils/path.mjs';
+import { exponentialBackoffRetry, BatchAPICall } from './utils/network.mjs';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const DATA_DIR = path.resolve(__dirname, '../data/raw')
-
-let response
+let response;
 
 response = await fetch('https://xkcd.com/info.0.json');
 const latestComicId = (await response.json())['num'];
 const comicIds = [...Array(latestComicId + 1).keys()].slice(1);
 
+const batchAPICall = new BatchAPICall();
+
 for await (const comicId of comicIds) {
-  const filePath = `${DATA_DIR}/${comicId}.json`
-  if(fs.existsSync(filePath)) {
-    console.log(`Comic ${comicId} already exists. Skipping.`)
-  } else {
-    console.log(`Fetching info for comic ${comicId}.`)
-    response = await fetch(`https://xkcd.com/${comicId}/info.0.json`);
-    // 🙏 https://stackoverflow.com/a/51302466/123545
-    const fileStream = fs.createWriteStream(filePath);
-    await new Promise((resolve, reject) => {
-      response.body.pipe(fileStream);
-      response.body.on("error", reject);
-      fileStream.on("finish", resolve);
-    });
+  const filePath = `${DATA_DIR}/${comicId}.json`;
+  if (fs.existsSync(filePath)) {
+    console.log(`Comic ${comicId} already exists. Skipping.`);
+    continue;
   }
+  if (comicId === 404) {
+    // id 404 is an April fools joke
+    const writer = fs.createWriteStream(filePath);
+    writer.write('{}');
+    continue;
+  }
+  const request = async () => {
+    const fetchInfo = async () => {
+      console.log(`Fetching info for comic ${comicId}.`);
+      const res = await fetch(`https://xkcd.com/${comicId}/info.0.json`);
+
+      if (!res.ok) throw new Error('Request failed!');
+      return res;
+    };
+
+    try {
+      const response = await exponentialBackoffRetry(fetchInfo, {
+        callback: ({ attempt, delayMs }) =>
+          console.log(
+            `Retry fetching info for comic ${comicId}: attempt ${attempt} after ${delayMs}ms`
+          ),
+      });
+      // 🙏 https://stackoverflow.com/a/51302466/123545
+      const fileStream = fs.createWriteStream(filePath);
+      await new Promise((resolve, reject) => {
+        response.body.pipe(fileStream);
+        response.body.on('error', reject);
+        fileStream.on('finish', resolve);
+      });
+      return `Comic info ${comicId} success`;
+    } catch (error) {
+      console.warn(`Error fetching explanation for comic ${comicId}`);
+      return `Comic info ${comicId} failed`;
+    }
+  };
+
+  batchAPICall.requestList.push(request);
 }
+
+batchAPICall.makeRequests();
